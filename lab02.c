9326804b74cb98bfe1e1739ffa8bfd2d5b1cc381
test <s6704062611107@email.kmutnb.ac.th>
2024-11-28 14:47:28 +0700

    Update lab02.c

--- lab02.c
+++ lab02.c
@@ -10,10 +10,10 @@ int main () {
 	scanf("%d",&digit2);
 	if (digit > digit2) {
 	    printf("N1 > N2\n");
-	   printf("%d",digit - digit2);
+	   printf("%s %d","Result N1 - N2 is :",digit - digit2);
 	} else if (digit < digit2) {
 	   printf("N1 < N2 \n");
-	   printf("%d",digit2 - digit);
+	   printf("%s %d","Result N1 - N2 is :",digit2 - digit);
 	}
 	return (0);
 };
