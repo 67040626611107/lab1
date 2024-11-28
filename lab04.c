6146290b4acdb120444550c2754b776917a83a1a
test <s6704062611107@email.kmutnb.ac.th>
2024-11-28 14:52:13 +0700

    Create lab04.c

---
+++ lab04.c
@@ -0,0 +1,18 @@
+#include <unistd.h> 
+#include <stdio.h> 
+int main () {
+	int digit;
+	int digit2;
+
+	printf("n1 \n n2");
+	scanf("%d",&digit);
+	scanf("%d",&digit2);
+	if (digit > digit2) {
+	    printf("N1 > N2\n");
+	   printf("%s %d","Result N1 - N2 is :",digit - digit2);
+	} else if (digit < digit2) {
+	   printf("N1 < N2 \n");
+	   printf("%s %d","Result N1 - N2 is :",digit2 - digit);
+	}
+	return (0);
+};
